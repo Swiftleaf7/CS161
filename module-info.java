cccf321559b3bdf3f201eaa3a2695ca211118855
Swiftleaf7 <swiftleaf3@gmail.com>
2025-04-10 10:25:57 -0700

    helloworld2.java

--- module-info.java
+++ module-info.java
@@ -1,8 +1,17 @@
-/**
- * 
- */
-/**
- * 
- */
-module cs161 {
+package Main;
+// create a local variable
+//use variable in an if statement
+// printout("hello world")
+
+
+public class helloworld {
+  public static void main(String[] args) {
+	  
+	  int a = 3;
+	  
+	  if(a < 4) {
+		  System.out.println("hello world");
+	  }
+		  
+  }
 }
