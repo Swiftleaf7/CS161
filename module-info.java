24d178f996e93c5a029a4fff498e26df3d672549
Swiftleaf7 <swiftleaf3@gmail.com>
2025-04-10 11:08:17 -0700

    helloworld3.java

--- module-info.java
+++ module-info.java
@@ -1,8 +1,22 @@
-/**
- * 
- */
-/**
- * 
- */
-module cs161 {
+package Main;
+// create a static variable before the string block
+// declare that static variable a = 3
+// create an if statement (a < 4)
+// printout("hello world")
+
+	
+	
+public class helloworld {
+	
+	static int a;
+	
+  public static void main(String[] args) {
+	  
+	  a = 3;
+	  
+	  if(a < 4) {
+		  System.out.println("Hello World!");
+	  }
+		  
+  }
 }
