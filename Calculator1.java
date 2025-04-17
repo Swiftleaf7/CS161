e0021f78d107b6dfa78366ac0b1cb1c92544ca82
Swiftleaf7 <swiftleaf3@gmail.com>
2025-04-17 10:54:58 -0700

    Create Calculator1.java

---
+++ Calculator1.java
@@ -0,0 +1,16 @@
+package cgcc.edu.cs161;
+
+public class Main {
+	
+	
+	static int a = 20;
+	static double b = 1.5;
+	
+	public static void main(String[] args) {
+			Main myObject = new Main();
+			myObject.calculator();
+	}	
+	public void calculator() {
+				System.out.println(a*b);
+			}
+}
