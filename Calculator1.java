91d831271fc27973f5716e6bd352f74c235c7306
Swiftleaf7 <swiftleaf3@gmail.com>
2025-04-22 09:25:57 -0700

    Update Calculator1.java

--- Calculator1.java
+++ Calculator1.java
@@ -1,16 +1,21 @@
 package cgcc.edu.cs161;
-
+// make an instance onto an object called calculator
 public class Main {
 	
-	
+	// make the variable for the calculations
 	static int a = 20;
+	// use a double variable for the non whole number
 	static double b = 1.5;
 	
 	public static void main(String[] args) {
-			Main myObject = new Main();
+	// maker the string and create thwe object for the instance variable	
+		Main myObject = new Main();
+		// call on "calculator" so that it prints out the answer
 			myObject.calculator();
 	}	
+	// craete the calculator to find out the product of 20x1.5 
 	public void calculator() {
 				System.out.println(a*b);
 			}
 }
+// 
